436f7c5d05322e996ed6c70c11601e16b9daa3a3
carlos romero paradis <carlos romero paradis>
2016-11-14 17:55:28 +0100

    Commit inicial

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+    public Transform player;
+
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        transform.position = new Vector3(0, player.position.y + 4, player.position.z-15);
+	}
+}
---
+++ Assets/Scripts/MoveShip.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveShip : MonoBehaviour {
+	
+	public float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetKey(KeyCode.RightArrow))
+			gameObject.transform.Translate(speed * Time.deltaTime, 0.0f, 0.0f);
+		if(Input.GetKey(KeyCode.LeftArrow))
+			gameObject.transform.Translate(-speed * Time.deltaTime, 0.0f, 0.0f);
+		if(Input.GetKey(KeyCode.UpArrow))
+			gameObject.transform.Translate(0.0f, 0.0f,speed * Time.deltaTime);
+
+	
+	}
+}
