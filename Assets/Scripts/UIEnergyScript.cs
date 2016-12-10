add87c817cc75683c744e164d060f020d450f4a6
Adrian Rambal <adrian.rambal@est.fib.upc.edu>
2016-12-10 13:57:29 +0100

    muestra energia en la UI

---
+++ Assets/Scripts/UIEnergyScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIEnergyScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    public GameObject nave;
+    private Text text;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        text.text = ((int)nave.GetComponent<MoveShip>().energy).ToString();
+    }
+    // Update is called once per frame
+
+}
