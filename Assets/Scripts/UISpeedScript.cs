4f87b92538d0f888b159ba9d3cca4d89cf380e93
Adrian Rambal <adrian.rambal@est.fib.upc.edu>
2016-12-10 12:18:50 +0100

    UI muestra velocidad

---
+++ Assets/Scripts/UISpeedScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UISpeedScript : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject nave;
+    private Text text;
+
+	void Start () {
+        text = GetComponent<Text>();
+	}
+	
+    void Update()
+    {
+        text.text = ((int)nave.GetComponent<MoveShip>().speed).ToString();
+    }
+	// Update is called once per frame
+	
+}
