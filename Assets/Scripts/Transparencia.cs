12a0997031e903e381fde657a012f0031edc8fe9
carlos romero paradis <carlos romero paradis>
2016-12-14 18:13:04 +0100

    Añadido circuito 1 (escena city)

---
+++ Assets/Scripts/Transparencia.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Transparencia : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Renderer>().enabled = false;
+    }
+}
